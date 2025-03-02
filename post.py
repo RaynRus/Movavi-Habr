f073173479f099116702c055f18923fc306f0f9b
RaynRus <aleksey@gorbunov.su>
2025-03-02 14:39:44 +0700

    02/03/25
    -Movavi Habr was created!

---
+++ post.py
@@ -0,0 +1,55 @@
+import sqlite3
+import datetime
+
+db_name = "data.db"
+
+
+def create_post_table():
+    SQL = """
+        CREATE TABLE IF NOT EXISTS post (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            text TEXT,
+            at_publish TEXT,
+            author_id INTEGER
+        )
+    """
+    con = sqlite3.connect(db_name)
+    con.execute(SQL)
+
+
+class Post:
+    def __init__(self, id, title, text, at_publish, author_id):
+        self.id = id
+        self.title = title
+        self.text = text
+        self.at_publish = at_publish
+        self.author_id = author_id
+
+    @staticmethod
+    def get_all():
+        SQL = "SELECT * FROM post"
+        con = sqlite3.connect(db_name)
+        q = con.execute(SQL)
+        data = q.fetchall()
+        return [Post(*row) for row in data]
+
+    @staticmethod
+    def get_by_author(author_id):
+        SQL = "SELECT * FROM post WHERE author_id = ?"
+        con = sqlite3.connect(db_name)
+        q = con.execute(SQL, [author_id])
+        data = q.fetchall()
+        return [Post(*row) for row in data]
+
+    @staticmethod
+    def create(title, text, author_id):
+        SQL = """
+            INSERT INTO post(title, text, at_publish, author_id)
+            VALUES (?, ?, ?, ?)
+        """
+        con = sqlite3.connect(db_name)
+        con.execute(SQL, [
+            title, text, datetime.datetime.now().strftime("%d.%m.%Y %H:%M"), author_id
+        ])
+        con.commit()
